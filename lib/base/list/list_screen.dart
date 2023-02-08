b372e4ad193c44b2192df25d625dbb8bfb377d2c
miyoshi-cq <t.miyoshi@caraquri.com>
2023-02-08 12:06:17 +0900

    modify onRefresh

--- lib/base/list/list_screen.dart
+++ lib/base/list/list_screen.dart
@@ -1,32 +1,38 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_architecture_template_ui/base/list/list_strategy.dart';
 
-class ListScreen<W extends Widget, T> extends StatelessWidget {
+class ListScreen<W extends Widget, T> extends StatefulWidget {
   final ListStrategy<W, T> strategy;
 
   const ListScreen(this.strategy);
 
   @override
+  State<ListScreen<W, T>> createState() => _ListScreenState<W, T>();
+}
+
+class _ListScreenState<W extends Widget, T> extends State<ListScreen<W, T>> {
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(strategy.title),
+        title: Text(widget.strategy.title),
       ),
       body: FutureBuilder<List<T>>(
-        future: strategy.fetch,
+        future: widget.strategy.fetch,
         builder: (context, snapshot) {
           return RefreshIndicator(
-            onRefresh: () {
-              return Future(() => null);
+            onRefresh: () async {
+              setState(() {});
             },
             child: ListView.builder(
               itemCount: snapshot.data?.length ?? 0,
               itemBuilder: (BuildContext context, int index) {
                 return GestureDetector(
                   onTap: () {
-                    strategy.onTap(context, index);
+                    widget.strategy.onTap(context, index);
                   },
-                  child: strategy.itemWidgetBuilder(snapshot.data![index]),
+                  child:
+                      widget.strategy.itemWidgetBuilder(snapshot.data![index]),
                 );
               },
             ),

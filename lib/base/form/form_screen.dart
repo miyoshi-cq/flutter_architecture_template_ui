c7f0694864a96715e1060fe0c00ef6754eca341a
miyoshi-cq <t.miyoshi@caraquri.com>
2023-02-14 11:43:36 +0900

    create validator

--- lib/base/form/form_screen.dart
+++ lib/base/form/form_screen.dart
@@ -1,31 +1,45 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_architecture_template_ui/base/form/form_strategy.dart';
 
-class FormScreen extends StatelessWidget {
+class FormScreen extends StatefulWidget {
   final FormStrategy strategy;
 
   const FormScreen(this.strategy);
 
   @override
+  State<FormScreen> createState() => _FormScreenState();
+}
+
+class _FormScreenState extends State<FormScreen> {
+  final formKey = GlobalKey<FormState>();
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(strategy.title),
+        title: Text(widget.strategy.title),
       ),
       body: Center(
         child: Container(
           padding: const EdgeInsets.all(16),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: strategy.views +
-                [
-                  TextButton(
-                    style: strategy.submitButtonStyle,
-                    onPressed: strategy.submit,
-                    child: Text(strategy.submitButtonTitle),
-                  ),
-                ],
+          child: Form(
+            key: formKey,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: widget.strategy.views +
+                  [
+                    TextButton(
+                      style: widget.strategy.submitButtonStyle,
+                      onPressed: () {
+                        if (formKey.currentState!.validate()) {
+                          widget.strategy.submit();
+                        }
+                      },
+                      child: Text(widget.strategy.submitButtonTitle),
+                    ),
+                  ],
+            ),
           ),
         ),
       ),

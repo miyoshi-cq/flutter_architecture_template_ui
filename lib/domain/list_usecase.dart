d76face629c2a3239310a23e8f4b41c4ccc55bea
miyoshi-cq <t.miyoshi@caraquri.com>
2023-02-21 14:03:00 +0900

    fix Usecase

--- lib/domain/list_usecase.dart
+++ lib/domain/list_usecase.dart
@@ -4,12 +4,15 @@ class ListUsecase<T extends Identifiable> {
   int lastId = 0;
   List<T> items = [];
 
-  Future<List<T>> get(Future<List<T>> repo, bool isAddtional) async {
+  Future<List<T>> get(
+    Future<List<T>> Function(int) repo,
+    bool isAddtional,
+  ) async {
     if (!isAddtional) {
       lastId = 0;
     }
 
-    final response = await repo;
+    final response = await repo(lastId);
 
     if (response.isNotEmpty) {
       lastId = response.last.id;

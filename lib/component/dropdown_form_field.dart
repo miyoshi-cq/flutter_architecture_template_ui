24bebacdeb35fcd380134feca8753986f8fbd230
miyoshi-cq <t.miyoshi@caraquri.com>
2023-02-24 18:14:10 +0900

    cleanup

--- lib/component/dropdown_form_field.dart
+++ lib/component/dropdown_form_field.dart
@@ -14,43 +14,55 @@ class _DropdownFormFieldState extends State<DropdownFormField> {
 
   @override
   Widget build(BuildContext context) {
-    return FormField<String>(
-      builder: (field) => InputDecorator(
-        decoration: InputDecoration(
-          labelText: "リスト",
-          filled: true,
-          fillColor: Colors.white,
-          border: OutlineInputBorder(
-            borderSide: BorderSide(
-              color: Colors.grey.shade400,
-            ),
-          ),
-          enabledBorder: OutlineInputBorder(
-            borderSide: BorderSide(
-              color: Colors.grey.shade400,
-            ),
-          ),
-          contentPadding: EdgeInsets.all(8),
-        ),
-        child: DropdownButtonHideUnderline(
-          child: DropdownButton<String>(
-            items: widget.items
-                .map((value) => DropdownMenuItem(
-                    value: value,
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [Text(value)],
-                    )))
-                .toList(),
-            onChanged: (value) {
-              setState(() {
-                _selected = value;
-              });
-            },
-            value: _selected,
-          ),
-        ),
-      ),
+    return DropdownFormFieldWidget(
+      widget.items,
+      _selected,
+      ((value) {
+        setState(() {
+          _selected = value;
+        });
+      }),
     );
   }
+}
+
+class DropdownFormFieldWidget extends FormField<String> {
+  DropdownFormFieldWidget(
+    List<String> items,
+    String? selected,
+    ValueChanged<String?> onChanged,
+  ) : super(
+          builder: (field) => InputDecorator(
+            decoration: InputDecoration(
+              labelText: "リスト",
+              filled: true,
+              fillColor: Colors.white,
+              border: OutlineInputBorder(
+                borderSide: BorderSide(
+                  color: Colors.grey.shade400,
+                ),
+              ),
+              enabledBorder: OutlineInputBorder(
+                borderSide: BorderSide(
+                  color: Colors.grey.shade400,
+                ),
+              ),
+              contentPadding: EdgeInsets.all(8),
+            ),
+            child: DropdownButtonHideUnderline(
+              child: DropdownButton<String>(
+                items: items
+                    .map((value) => DropdownMenuItem(
+                        value: value,
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [Text(value)],
+                        )))
+                    .toList(),
+                onChanged: onChanged,
+                value: selected,
+              ),
+            ),
+          ),
+        );
 }
